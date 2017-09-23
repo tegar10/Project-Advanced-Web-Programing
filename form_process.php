f21e5ad71d0a7201f3a22ed746e221a0216f861c
tegar10 <32199464+tegar10@users.noreply.github.com>
2017-09-23 22:41:48 +0700

    Add files via upload

---
+++ form_process.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_POST['submit'])){
+
+	$name = array("alexwawo", "maria", "johndoe", "janedoe", "nathan");
+
+	$minimum = 5;
+	$maximum = 10;
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(strlen($username) < $minimum){
+		echo "Username harus memiliki panjang 5 atau lebih";
+	}
+
+	if(strlen($username) > $maximum){
+		echo "Username tidak boleh lebih panjang dari 10";
+	}
+
+	if(!in_array($username, $name)){
+		echo "Sorry, access denied";
+	}else{
+		echo "W e l c o m e . . . .";
+	}
+}
+?>
